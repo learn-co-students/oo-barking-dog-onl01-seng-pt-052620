294592fe3af96f663a9b641775971f51b56ca2d8
github username <a.gaysenko@gmail.com>
2020-06-09 23:16:22 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Dog
+    def name=(dogs_name)
+        @calling_dog = dogs_name
+    end
+
+    def name
+        @calling_dog = "Fido" #set variable to an actual name
+    end
+        
+    def bark
+        puts "woof!"
+    end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+
+
+
+
+
+
+
